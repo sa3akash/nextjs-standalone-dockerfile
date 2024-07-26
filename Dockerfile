390b1fe51dbb4491b08398221e47af296c264c88
sa3akash <sa3akash@gmail.com>
2024-07-26 19:33:52 +0600

    all added

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:20-alpine AS deps
+
+COPY package*.json ./
+
+RUN npm ci
+
+FROM node:20-alpine AS builder
+
+COPY . .
+COPY --from=deps /node_modules ./node_modules
+RUN npm run build
+
+FROM node:20-alpine AS runner
+
+COPY --from=builder /.next/standalone ./
+# COPY --from=builder /public ./public
+# COPY --from=builder /.next/static ./.next/static
+
+EXPOSE 3001
+
+ENV PORT 3001
+
+CMD HOSTNAME="0.0.0.0" node server.js
---
+++ app/(root)/page.tsx
@@ -0,0 +1,28 @@
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+import React from "react";
+
+const Home = () => {
+
+
+  return (
+    <div className="mt-9 flex flex-col gap-9">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map((item, index) => (
+            <PodcastCard
+              key={index}
+              description={item.description}
+              id={item.id}
+              imgURL={item.imgURL}
+              title={item.title}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
