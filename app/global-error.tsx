16e738de5f6c6d1e9311137eab94bd37691827e5
sa3akash <sa3akash@gmail.com>
2024-07-27 18:55:09 +0600

    update not found page

---
+++ app/global-error.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="text-4xl h-full flex items-center justify-center text-rose-400">Error</div>
+  );
+};
+
+export default Error;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFount = () => {
   return (
-    <div className="text-4xl h-full flex items-center justify-center">404</div>
+    <div className="text-4xl h-screen flex items-center justify-center">404 Not Found!</div>
   );
 };
 
