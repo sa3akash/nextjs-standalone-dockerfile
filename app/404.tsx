c1e7514cbe554270af902830f4717baaa27c92e9
sa3akash <sa3akash@gmail.com>
2024-07-27 17:51:38 +0600

    update 404

---
+++ app/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFount = () => {
+  return (
+    <div className="text-4xl h-full flex items-center justify-center">404</div>
+  );
+};
+
+export default NotFount;
